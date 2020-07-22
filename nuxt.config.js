20bce36a2318053dd36a47d4cb383cdb4059c0a3
manak1 <mana23happy@hotmail.co.jp>
2020-07-23 03:49:32 +0900

    :bug: タイポの修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: ['~/plugin/firebase.js'],
+  plugins: ['~/plugins/firebase.js'],
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
