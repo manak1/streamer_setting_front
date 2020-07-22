8e72388bf9305a00c8cbde011096c71e32abdab0
manak1 <mana23happy@hotmail.co.jp>
2020-07-23 04:14:09 +0900

    :art: ヘッダーのコーディング

---
+++ components/layout/lHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <header class="w-full p-2 shadow-xl z-50">
+    <div class="container mx-auto flex items-center justify-between">
+      <img src="https://via.placeholder.com/60" class="rounded-full" alt="" />
+      <div class="flex space-x-3 items-center">
+        <ul class="space-x-6 hidden sm:flex">
+          <li class="p-2">
+            <nuxt-link to="/">
+              ホーム
+            </nuxt-link>
+          </li>
+          <li class="p-2">
+            <nuxt-link to="/fortnite">
+              配信者一覧
+            </nuxt-link>
+          </li>
+          <li class="p-2">ゲーム一覧</li>
+        </ul>
+        <div class="hidden sm:block">
+          <input
+            type="text"
+            class="w-64 p-3"
+            placeholder="ストリーマーの名前を入力"
+          />
+        </div>
+        <div class="block sm:hidden">
+          メニュー
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
+  <div class="overflow-hidden">
+    <l-header />
     <Nuxt />
   </div>
 </template>
+
+<style lang="scss">
+body {
+  background-color: #f5f6f8;
+}
+</style>
