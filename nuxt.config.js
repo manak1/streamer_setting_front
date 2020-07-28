dfd4d356603370fead47e8608cfc37250f11ab0d
manak1 <mana23happy@hotmail.co.jp>
2020-07-29 02:58:06 +0900

    :penguin: env設定の追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,6 +51,16 @@ export default {
     '@nuxtjs/tailwindcss',
     '@nuxtjs/dotenv',
   ],
+  env: {
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    DATABASE_URL: process.env.DATABASE_URL,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+    MESSAGING_SENDERID: process.env.MESSAGING_SENDERID,
+    APP_ID: process.env.APP_ID,
+    MEASUREMENT_ID: process.env.MEASUREMENT_ID,
+  },
   /*
    ** Nuxt.js modules
    */
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,14 +1,16 @@
 import firebase from 'firebase'
 
-if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDERID,
-  })
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDERID,
 }
+/* 
+if (!firebase.apps.length) {
+  firebase.initializeApp({})
+} */
 
-export default firebase
+export default firebase.initializeApp(firebaseConfig)
