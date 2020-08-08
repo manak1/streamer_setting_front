836ddeb1220460cde3b7373a8c1457a41256c107
manak1 <mana23happy@hotmail.co.jp>
2020-08-08 23:18:07 +0900

    :art: コンテナの調整

--- components/common/cGearList.vue
+++ components/common/cGearList.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="mx-auto -mt-10 lg:container-gear-lg">
+  <section class="mx-auto -mt-10 lg:container">
     <h2 class="text-center text-xl md:text-2xl font-bold">
       {{ name }}の使用デバイス一覧
     </h2>
