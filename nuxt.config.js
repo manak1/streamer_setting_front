c8858f482df793895a0baa49561f642a4de65340
manak1 <mana23happy@hotmail.co.jp>
2020-08-12 00:25:21 +0900

    :penguin: サイトタイトルの変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: 'StreamerSettings | 配信者の設定デバイスを知ろう' || '',
+    title: 'StreamerSettings | 配信者の使用設定、デバイスを知ろう' || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
