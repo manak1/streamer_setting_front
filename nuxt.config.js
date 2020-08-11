c44e12aeab9297090977ae0ca4301877ec6cbf05
manak1 <mana23happy@hotmail.co.jp>
2020-08-12 01:11:10 +0900

    :penguin: エラーの回避

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,5 +69,14 @@ export default {
    ** Build configuration
    ** See https://nuxtjs.org/api/configuration-build/
    */
-  build: {},
+  build: {
+    extend(config, { isServer }) {
+      if (isServer) {
+        config.externals = {
+          '@firebase/app': 'commonjs @firebase/app',
+          '@firebase/firestore': 'commonjs @firebase/firestore',
+        }
+      }
+    },
+  },
 }
