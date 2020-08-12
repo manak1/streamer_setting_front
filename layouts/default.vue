23bfd03244ccf66248e748ca3a82092b75986964
manak1 <mana23happy@hotmail.co.jp>
2020-08-12 18:47:52 +0900

    :art: フォントファミリーに遊ゴシックを指定

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,13 +3,18 @@
     <l-header />
     <!-- <div style="padding-top: 76px;"></div> -->
     <Nuxt />
-    <l-footer-box />
+    <!--   <l-footer-box /> -->
     <div class="md:hidden" style="padding-bottom: 58px;" />
   </div>
 </template>
 
 <style lang="scss">
 body {
+  max-width: 100%;
+  overflow-y: scroll;
+  overflow-x: hidden;
   background-color: #f5f6f8;
+  font-family: '游ゴシック体', YuGothic, '游ゴシック Medium', 'Yu Gothic Medium',
+    '游ゴシック', 'Yu Gothic', sans-serif;
 }
 </style>
